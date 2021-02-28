c73284cdf483c4c46cabc7fc60ba3c051dabec20
Yanjun <kingfisherzh@gmail.com>
2021-02-27 23:17:38 -0500

    1st edit

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -74,11 +74,11 @@ while True:
     else:
         backlight.value = True  # turn on backlight
     if buttonB.value and not buttonA.value:  # just button A pressed
-        display.fill(screenColor) # set the screen to the users color
+        disp.fill(screenColor) # set the screen to the users color
     if buttonA.value and not buttonB.value:  # just button B pressed
-        display.fill(color565(255, 255, 255))  # set the screen to white
+        disp.fill(color565(255, 255, 255))  # set the screen to white
     if not buttonA.value and not buttonB.value:  # none pressed
-        display.fill(color565(0, 255, 0))  # green
+        disp.fill(color565(0, 255, 0))  # green
 
 
     # Draw a black filled box to clear the image.
