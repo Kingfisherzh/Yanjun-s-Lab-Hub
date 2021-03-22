95528f6194e3654dcb787aa5a951d31c4370e3ae
Toki <kingfisherzh@gmail.com>
2021-03-21 21:51:03 -0400

    Update try.py

--- Lab 3/demo/try.py
+++ Lab 3/demo/try.py
@@ -9,6 +9,10 @@ import signal
 import sys
 from queue import Queue
 
+from vosk import Model, KaldiRecognizer
+import sys
+import os
+import wave
  
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
