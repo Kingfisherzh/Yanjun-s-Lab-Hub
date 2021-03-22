4c0a7218666c60c5aa323df1784978ebc2550be5
Toki <kingfisherzh@gmail.com>
2021-03-21 21:50:33 -0400

    Update try.py

--- Lab 3/demo/try.py
+++ Lab 3/demo/try.py
@@ -13,42 +13,31 @@ from queue import Queue
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
-hostname = socket.gethostname()
-hardware = 'plughw:2,0'
+if not os.path.exists("model"):
+    print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
+    exit (1)
 
-app = Flask(__name__)
-socketio = SocketIO(app)
-audio_stream = Popen("/usr/bin/cvlc alsa://"+hardware+" --sout='#transcode{vcodec=none,acodec=mp3,ab=256,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:8080/}' --no-sout-all --sout-keep", shell=True)
+wf = wave.open(sys.argv[1], "rb")
+if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print ("Audio file must be WAV format mono PCM.")
+    exit (1)
 
-@socketio.on('speak')
-def handel_speak(val):
-    call(f"espeak '{val}'", shell=True)
+model = Model("model")
+# You can also specify the possible word list
+rec = KaldiRecognizer(model, wf.getframerate(), "zero oh one two three four five six seven eight nine [unk]")
 
-@socketio.on('connect')
-def test_connect():
-    print('connected')
-    emit('after connect',  {'data':'Lets dance'})
+while True:
+    data = wf.readframes(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+        print(mpu.acceleration)
+    else:
+        print(rec.PartialResult())
 
-@socketio.on('ping-gps')
-def handle_message(val):
-    # print(mpu.acceleration)
-    emit('pong-gps', mpu.acceleration) 
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', hostname=hostname)
-
-def signal_handler(sig, frame):
-    print('Closing Gracefully')
-    audio_stream.terminate()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
+print(rec.FinalResult())
 
 
-if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=5000)
 
 
