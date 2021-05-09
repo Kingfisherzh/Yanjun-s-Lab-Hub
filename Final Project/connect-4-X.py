47eb0233a1ee4890b064383bed7d24acdb6af525
Toki <kingfisherzh@gmail.com>
2021-05-09 18:05:59 -0400

    1

--- Final Project/connect-4-X.py
+++ Final Project/connect-4-X.py
@@ -140,7 +140,7 @@ def get_next_open_row(board, col):
 			return r
 
 def print_board(board):
-	for i in range(ROW_COUNT):
+	for i in reversed(range(ROW_COUNT)):
 		print(board[i])
 
 def winning_move(board, piece):
